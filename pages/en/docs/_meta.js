0b834126f13f25dedbd009752c7ed81113b2b535
Dushusir <1414556676@qq.com>
2025-06-10 21:14:51 +0800

    docs: new article

--- pages/en/docs/_meta.js
+++ pages/en/docs/_meta.js
@@ -1,4 +1,9 @@
 export default {
   index: 'Introduction',
   installation: 'Installation',
+  amazon: 'Amazon Tutorial',
+  x: 'X Tutorial',
+  reddit: 'Reddit Tutorial',
+  tiktok: 'TikTok Tutorial',
+  instagram: 'Instagram Tutorial'
 };
