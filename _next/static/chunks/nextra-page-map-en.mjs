001f75dc582b4ba7f38f0b8067785b7c1db12daa
Dushusir <Dushusir@users.noreply.github.com>
2025-06-11 10:09:49 +0000

    deploy: c7965fea532cb8d0cc73532545b2c3ac82c7f34c

--- _next/static/chunks/nextra-page-map-en.mjs
+++ _next/static/chunks/nextra-page-map-en.mjs
@@ -8,16 +8,46 @@ export const pageMap = [{
   children: [{
     data: en_docs_meta
   }, {
+    name: "amazon",
+    route: "/en/docs/amazon",
+    frontMatter: {
+      "sidebarTitle": "Amazon"
+    }
+  }, {
     name: "index",
     route: "/en/docs",
     frontMatter: {
       "sidebarTitle": "Index"
     }
   }, {
+    name: "instagram",
+    route: "/en/docs/instagram",
+    frontMatter: {
+      "sidebarTitle": "Instagram"
+    }
+  }, {
     name: "installation",
     route: "/en/docs/installation",
     frontMatter: {
       "sidebarTitle": "Installation"
+    }
+  }, {
+    name: "reddit",
+    route: "/en/docs/reddit",
+    frontMatter: {
+      "sidebarTitle": "Reddit"
+    }
+  }, {
+    name: "tiktok",
+    route: "/en/docs/tiktok",
+    frontMatter: {
+      "sidebarTitle": "Tiktok"
+    }
+  }, {
+    name: "x",
+    route: "/en/docs/x",
+    frontMatter: {
+      "sidebarTitle": "X"
     }
   }]
 }, {
